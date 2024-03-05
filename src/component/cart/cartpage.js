f1ac81ff39a9f3be7a4e4fbc08f136da6ae64823
Hager-osama <hagerelsha3er@gmail.com>
2024-03-05 13:54:34 +0200

    nw

--- src/component/cart/cartpage.js
+++ src/component/cart/cartpage.js
@@ -1,27 +1,56 @@
 import React from "react";
 import Cartpagecomponent from './cartpagecomponent'
 import { useShoppingCart } from "../context/shoppingCartContext";
-
+import { Col, Container, Row } from "react-bootstrap";
+import NavBar from "../navbar/NavBar";
 const Cartpage = () => {
   const {
     cartQuantity,
     cartItems,
-
     increaseCartQuantity,
     decreaseCartQuantity,
   } = useShoppingCart();
 
   return (
     <>
-      {" "}
+        <NavBar />
+        <Container>
+        <Row>
+          <Col>
+            <div className="d-flex ">
+              <h2>Cart </h2>
+              <div
+                className="rounded-circle d-flex justify-content-center align-items-center m-1 "
+                style={{
+                  color: "white",
+                  width: "1.60rem",
+                  height: "1.60rem",
+                  transform: "translate(25%, 25%)",
+                  alignItems: "center",
+                  backgroundColor: "#000",
+                }}
+              >
+                {cartQuantity}
+              </div>
+            </div>
+          </Col>
+
+          <Col md="auto" className="text-muted">
+            <h3>Qty</h3>
+          </Col>
+
+          <Col xs lg="2" className="text-muted">
+            <h3>Total</h3>
+          </Col>
+        </Row>
+        </Container>
       {cartItems.map((item) => (
         <Cartpagecomponent
           key={item.mealId._id}
           item={item}
           increaseCartQuantity={increaseCartQuantity}
           decreaseCartQuantity={decreaseCartQuantity}
-          cartQuantity={cartQuantity}
-         
+          cartQuantity={cartQuantity} 
         />
       ))}{" "}
     </>
--- src/component/cart/cartpagecomponent.js
+++ src/component/cart/cartpagecomponent.js
@@ -1,55 +1,22 @@
 import React from "react";
-import NavBar from "../navbar/NavBar";
+
 import { Col, Container, Row, Button } from "react-bootstrap";
 import FormateCurrency from "../flashDeals/formateCurrency";
 
-const Cartpagecomponent = (
+const Cartpagecomponent = ({
   item,
   decreaseCartQuantity,
   increaseCartQuantity,
   cartQuantity,
-
-) => {
-
-
+}) => {
   return (
     <div
       style={{
         backgroundColor: "#e7d6c2",
-        height: "5000px",
       }}
     >
-      <NavBar />
-      <Container className="   ">
-        <Row>
-          <Col>
-            <div className="d-flex ">
-              <h2>Cart </h2>
-              <div
-                className="rounded-circle d-flex justify-content-center align-items-center m-1 "
-                style={{
-                  color: "white",
-                  width: "1.60rem",
-                  height: "1.60rem",
-                  transform: "translate(25%, 25%)",
-                  alignItems: "center",
-                  backgroundColor: "#000",
-                }}
-              >
-                {cartQuantity}
-              </div>
-            </div>
-          </Col>
-
-          <Col md="auto" className="text-muted">
-            <h3>Qty</h3>
-          </Col>
-
-          <Col xs lg="2" className="text-muted">
-            <h3>Total</h3>
-          </Col>
-        </Row>
-
+    
+      <Container className="  ">
         <Row>
           <Col>
             <img
@@ -63,7 +30,7 @@ const Cartpagecomponent = (
           </Col>
           <Col md="auto">
             <Button onClick={() => decreaseCartQuantity(item.mealId)}>-</Button>
-            <span className="fs-4"> {item.quantity}  </span>
+            <span className="fs-4"> {item.quantity} </span>
             <Button onClick={() => increaseCartQuantity(item.mealId)}>+</Button>
           </Col>
           <Col xs lg="2">
